5bfef16f3c2964d12750a4a9c7a623c03ad25008
Ayoub <ayoub.fiadh@gmail.com>
2023-09-25 22:08:53 -0400

    Fix bug client import

--- sveltekit-charts/src/routes/+page.svelte
+++ sveltekit-charts/src/routes/+page.svelte
@@ -1,1 +1,21 @@
+<script>
+  import { chart } from "svelte-apexcharts?client";
+
+  let options = {
+    chart: {
+      type: "bar",
+    },
+    series: [
+      {
+        name: "sales",
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      },
+    ],
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+  };
+</script>
+
 <h1 class="text-3xl font-bold mt-10 text-center">Sveltekit - Chart</h1>
+<div use:chart={options} class="w-[400px]" />

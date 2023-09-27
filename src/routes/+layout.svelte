d1aa0116bfe8a347cbeef0133efe1b68ec5b1990
Ayoub <ayoub.fiadh@gmail.com>
2023-09-26 20:18:54 -0400

    Create header & Add darkMode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,40 @@
 <script>
+  import Header from "./Header.svelte";
   import "../app.css";
+  import { browser } from "$app/environment";
+
+  let darkMode = false;
+
+  if (browser) {
+    setTheme();
+  }
+
+  console.log('coucou');
+  function setTheme() {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      darkMode = true;
+    } else {
+      document.documentElement.classList.remove("dark");
+      darkMode = false;
+    }
+  }
 </script>
 
-<div class="app font-red-hat-text">
-  <main>
+<div class="app">
+  <Header bind:darkMode on:toggleTheme={setTheme} />
+  <main class="dark:bg-gray-900 h-full">
     <slot />
   </main>
 </div>
+<style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,49 @@
+<script>
+  import Fa from "svelte-fa/src/fa.svelte";
+  import { faBars, faMoon, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
+  import { page } from "$app/stores";
+  import { createEventDispatcher } from "svelte";
+
+  export let darkMode;
+
+  let openMenu = false;
+  let openMenuLink = false;
+  let innerWidth = 0;
+
+  const dispatch = createEventDispatcher();
+  function toggleTheme() {
+    darkMode = !darkMode;
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+    dispatch("toggleTheme");
+  }
+
+  function toggleMenu() {
+    openMenu = !openMenu;
+  }
+  function toggleMenuLink() {
+    openMenuLink = !openMenuLink;
+  }
+</script>
+
+<svelte:window bind:innerWidth />
+<header>
+  <nav class="antialiased h-12 bg-gray-100 dark:text-white border-gray-200 dark:bg-black">
+    <div class="flex flex-wrap items-center justify-between mx-auto px-8 h-full">
+      <!-- Logo -->
+      <div class=" flex items-center w-full">
+        {#if innerWidth < 900}
+          <button on:click={toggleMenuLink}>
+            <Fa icon={faBars} />
+          </button>
+        {/if}
+        <h1 class="grow flex items-center justify-center font-medium text-lg">Dashboard</h1>
+        <button
+          on:click={toggleTheme}
+          class="{!darkMode ? '' : 'text-yellow-500'} ml-4 w-[24px]"
+        >
+          <Fa icon={!darkMode ? faMoon : faSun} />
+        </button>
+      </div>
+    </div>
+  </nav>
+</header>

126aab9ad3d8b0045227847668ac6428a4079ac8
Ayoub <ayoub.fiadh@gmail.com>
2023-09-26 20:56:55 -0400

    Add reports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+  import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
   import { chart } from "svelte-apexcharts?client";
+  import Fa from "svelte-fa/src/fa.svelte";
 
   let options = {
     chart: {
@@ -15,7 +17,27 @@
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
     },
   };
-</script>
 
+  let reports = [
+    { title:'report 1', icon: faCalendarAlt, price: 400.01},
+    { title:'report 2', icon: faCalendarAlt, price: 680.01},
+    { title:'report 3', icon: faCalendarAlt, price: 10.00},
+    { title:'report 4', icon: faCalendarAlt, price: 1400.01}
+  ];
+</script>
+<div class="w-full grid grid-cols-4 gap-4 p-2">
+{#each reports as report}
+  <div class="p-2 border-l-8 border-green-800 shadow-xl rounded-lg">
+    <div class="flex justify-center h-22 items-center text-green-800">
+      <div class="text-xl uppercase font-medium ">{report.title}</div>
+      <div class="grow"/>
+      <Fa icon={report.icon} />
+    </div>
+    <div class="text-xs text-start mx-2">
+      $ {report.price}
+    </div>
+  </div>
+{/each}
+</div>
 <h1 class="text-3xl font-bold mt-10 text-center">Sveltekit - Chart</h1>
 <div use:chart={options} class="w-[400px]" />

e8c69389180b171542ad029d79e4aef8094b1fbb
Ayoub <ayoub.fiadh@gmail.com>
2023-12-23 23:12:26 -0500

    Fix responsive emails page

--- src/routes/emails/+page.svelte
+++ src/routes/emails/+page.svelte
@@ -1,88 +1,112 @@
 <script>
   import Fa from "svelte-fa/src/fa.svelte";
-  import { faBackward, faChevronLeft, faChevronRight, faEllipsisH, faEnvelope, faFile, faFolder, faMessage, faPaperPlane, faReply, faStar, faTag, faTrash, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+  import { faBackward, faBars, faChevronLeft, faChevronRight, faEllipsisH, faEnvelope, faFile, faFolder, faMessage, faPaperPlane, faReply, faStar, faTag, faTrash, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 
+  let activeSide = false;
+  let innerWidth = 0;
+  let activeContent = false;
+
+  $:if (innerWidth > 900) {
+    activeSide = true;
+    activeContent = true;
+  } else {
+    activeSide = false;
+    activeContent = false;
+  }
 </script>
+
+<svelte:window bind:innerWidth />
 <div class="email-app flex h-[calc(100vh-48px)] bg-white dark:bg-accent-dark-base">
-  <div class="email-side-nav w-[250px] flex flex-col transition-all duration-300 ease-in-out border-r border-r-gray-300 dark:border-r-white">
-    <div class="p-4 w-full text-center">
-      <a href="/" class=" px-4 py-2 rounded-lg bg-secondary-base text-white">New Message</a>
+  {#if activeSide}
+    <div class="email-side-nav flex flex-col transition-all duration-300 ease-in-out border-r border-r-gray-300 dark:border-r-white">
+      <div class="p-4 w-full text-center">
+        <a href="/" class=" px-4 py-2 rounded-lg bg-secondary-base text-white">New Message</a>
+      </div>
+      <div class="grow py-4 text-gray-500 dark:text-white">
+        <ul class="flex items-center flex-col p-0 w-full">
+          <li class="w-full">
+            <a href="/" class="nav-item">
+              <Fa icon={faEnvelope} class="w-6"/>
+              <div class="text-f6">Inbox</div>
+              <div class="grow"></div>
+              <div class="rounded-full bg-primary-lighten5 text-primary-base px-2">
+                <p>+99</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="p-0 w-full">
+            <a href="/" class="nav-item">
+              <Fa icon={faPaperPlane} class="w-6"/>
+              <div class="text-f6">Send</div>
+              <div class="grow"></div>
+              <div class="rounded-full bg-green-200 text-green-900 px-2">
+                <p>12</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="p-0 w-full">
+            <a href="/" class="nav-item">
+              <Fa icon={faStar} class="w-6"/>
+              <div class="text-f6">Important</div>
+              <div class="grow"></div>
+              <div class="rounded-full bg-blue-200 text-blue-900 px-2">
+                <p>3</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="p-0 w-full">
+            <a href="/" class="nav-item">
+              <Fa icon={faFile} class="w-6"/>
+              <div class="text-f6">Drafts</div>
+              <div class="grow"></div>
+              <div class="rounded-full bg-yellow-200 text-yellow-900 px-2">
+                <p>5</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="p-0 w-full">
+            <a href="/" class="nav-item">
+              <Fa icon={faTriangleExclamation} class="w-6"/>
+              <div class="text-f6">Spam</div>
+              <div class="grow"></div>
+              <div class="rounded-full bg-red-200 text-red-900 px-2">
+                <p>1</p>
+              </div>
+            </a>
+          </li>
+
+          <li class="p-0 w-full">
+            <a href="/" class="nav-item">
+              <Fa icon={faTrash} class="w-6"/>
+              <div class="text-f6">Trash</div>
+              <div class="grow"></div>
+              <div class="rounded-full bg-red-200 text-red-900 px-2">
+                <p>+99</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="grow py-4 text-gray-500 dark:text-white">
-      <ul class="flex items-center flex-col p-0 w-full">
-        <li class="w-full">
-          <a href="/" class="nav-item">
-            <Fa icon={faEnvelope} class="w-6"/>
-            <div class="text-f6">Inbox</div>
-            <div class="grow"></div>
-            <div class="rounded-full bg-primary-lighten5 text-primary-base px-2">
-              <p>+99</p>
-            </div>
-          </a>
-        </li>
-
-        <li class="p-0 w-full">
-          <a href="/" class="nav-item">
-            <Fa icon={faPaperPlane} class="w-6"/>
-            <div class="text-f6">Send</div>
-            <div class="grow"></div>
-            <div class="rounded-full bg-green-200 text-green-900 px-2">
-              <p>12</p>
-            </div>
-          </a>
-        </li>
-
-        <li class="p-0 w-full">
-          <a href="/" class="nav-item">
-            <Fa icon={faStar} class="w-6"/>
-            <div class="text-f6">Important</div>
-            <div class="grow"></div>
-            <div class="rounded-full bg-blue-200 text-blue-900 px-2">
-              <p>3</p>
-            </div>
-          </a>
-        </li>
-
-        <li class="p-0 w-full">
-          <a href="/" class="nav-item">
-            <Fa icon={faFile} class="w-6"/>
-            <div class="text-f6">Drafts</div>
-            <div class="grow"></div>
-            <div class="rounded-full bg-yellow-200 text-yellow-900 px-2">
-              <p>5</p>
-            </div>
-          </a>
-        </li>
-
-        <li class="p-0 w-full">
-          <a href="/" class="nav-item">
-            <Fa icon={faTriangleExclamation} class="w-6"/>
-            <div class="text-f6">Spam</div>
-            <div class="grow"></div>
-            <div class="rounded-full bg-red-200 text-red-900 px-2">
-              <p>1</p>
-            </div>
-          </a>
-        </li>
-
-        <li class="p-0 w-full">
-          <a href="/" class="nav-item">
-            <Fa icon={faTrash} class="w-6"/>
-            <div class="text-f6">Trash</div>
-            <div class="grow"></div>
-            <div class="rounded-full bg-red-200 text-red-900 px-2">
-              <p>+99</p>
-            </div>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
+  {/if}
   <div class="email-wrapper flex transition-all duration-300 ease-in-out grow ">
-    <div class="email-list flex flex-col w-2/5  border-r border-r-gray-300 dark:border-r-white">
+    <div
+      class="{innerWidth > 900 ? 'w-2/5' : activeContent ? 'hidden' :  'w-full'} email-list flex flex-col  border-r border-r-gray-300 dark:border-r-white"
+    >
       <div class="p-3 w-full border-b border-b-gray-300 dark:border-b-white">
         <div class="flex items-center justify-center w-full text-black dark:text-white">
           <div class="flex items-center justify-center gap-1">
+            <button
+              type="button"
+              on:click={()=> activeSide = !activeSide}
+              class="icon_item lg:!hidden"
+            >
+              <Fa icon={faBars} />
+            </button>
             <div class="icon_item">
               <Fa icon={faFolder} />
             </div>
@@ -111,7 +135,11 @@
       </div>
       <div class="email-list grow overflow-y-scroll">
         {#each {length: 12} as _}
-          <div class="p-3 w-full border-b border-b-gray-300 dark:border-b-white">
+          <button
+            type="button"
+            on:click={()=> activeContent = !activeContent}
+            class="p-3 w-full border-b border-b-gray-300 dark:border-b-white"
+          >
             <div class="flex items-center relative w-full">
               <input type="checkbox" class="w-8">
               <div class="grow flex flex-col items-start truncate">
@@ -123,37 +151,46 @@
                 <p>1 min ago</p>
               </div>
             </div>
-          </div>
+          </button>
         {/each}
         <div class="email-list-item">
 
         </div>
       </div>
     </div>
-    <div class="email-content grow">
-      <div class="p-8 flex items-center gap-5  border-b border-b-gray-300 dark:border-b-white">
-        <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Moo Joe" class="w-12 h-12 rounded-full">
-        <div>
-          <p>Nov, 02 2017</p>
-          <h3>Moo Doe</h3>
-          <p>To: email@mail.com</p>
+    {#if activeContent}
+      <div class="email-content grow relative">
+        <button
+          type="button"
+          on:click={()=> activeContent = !activeContent}
+          class="icon_item absolute top-2 left-2"
+        >
+          <Fa icon={faChevronLeft} />
+        </button>
+        <div class="p-8 flex items-center gap-5  border-b border-b-gray-300 dark:border-b-white">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Moo Joe" class="w-12 h-12 rounded-full">
+          <div>
+            <p>Nov, 02 2017</p>
+            <h3>Moo Doe</h3>
+            <p>To: email@mail.com</p>
+          </div>
+          <div class="grow"></div>
+          <div class="w-12 h-12 shadow-3xl rounded-full bg-orange-600 flex items-center justify-center text-white text-f4">
+            <Fa icon={faReply} />
+          </div>
         </div>
-        <div class="grow"></div>
-        <div class="w-12 h-12 shadow-3xl rounded-full bg-orange-600 flex items-center justify-center text-white text-f4">
-          <Fa icon={faReply} />
+        <div class="p-8">
+          <h3>
+            Title of this email goes here
+          </h3>
+          <p class="text-f5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+          </p>
         </div>
       </div>
-      <div class="p-8">
-        <h3>
-          Title of this email goes here
-        </h3>
-        <p class="text-f5">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-        </p>
-      </div>
-    </div>
+    {/if}
   </div>
 </div>
 
@@ -162,6 +199,6 @@
     @apply flex items-center justify-center gap-3 px-3 py-1 hover:bg-black/5
   }
   .icon_item {
-    @apply h-8 w-8 bg-accent-darken1 dark:bg-accent-dark-base flex items-center justify-center shadow-3xl;
+    @apply h-8 w-8 bg-slate-300 dark:bg-accent-dark-base flex items-center justify-center shadow-3xl;
   }
 </style>
